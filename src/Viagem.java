341894465c6399705c3ec9eaebc79de9d7df6424
Rafeixons <goncalvesrafeixons@gmail.com>
2026-04-22 21:41:46 -0300

    Regras menu

--- src/Viagem.java
+++ src/Viagem.java
@@ -1,6 +1,7 @@
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import javax.swing.JOptionPane;
 
 public class Viagem {
@@ -10,9 +11,9 @@ public class Viagem {
     String valor;
 
     LocalDate data;
+    LocalDate hoje;
 
     public void menu() {
-
 
         String[] opcoes = {"Planejar Viagem", "Sair"};
         int escolha = JOptionPane.showOptionDialog(
@@ -33,11 +34,20 @@ public class Viagem {
                 String entrada = JOptionPane.showInputDialog(null, "Qual a data da viagem? (dd/MM/yyyy)");
 
                 try {
-                    data = LocalDate.parse(entrada,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                    data = LocalDate.parse(entrada, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, "Data incorreta");
                 }
 
+                if (data.isBefore(hoje)) {
+                    JOptionPane.showMessageDialog(null, "A viagem já passou");
+                } else if (data.isEqual(hoje)) {
+                    JOptionPane.showMessageDialog(null, "A viagem é hoje!!");
+                } else {
+                    long diasFalta = ChronoUnit.DAYS.between(hoje, data);
+                    JOptionPane.showMessageDialog(null, "Faltam " + diasFalta + "Para sua viageeem!");
+                }
+                // colocar looping pra data incorreta
             } while (data == null);
             do {
                 dias = JOptionPane.showInputDialog(null, "Planeja ficar quantos dias em viagem?");
@@ -48,36 +58,34 @@ public class Viagem {
 
             JOptionPane.showMessageDialog(null, "Viagem cadastrada, obrigada por utilizar o nosso sistema!");
 
-            int diasI = Integer.parseInt(dias);
 
             double valorI = Double.parseDouble(valor);
 
-//            imprimir();
+            imprimir();
         } else if (escolha == 1) {
             JOptionPane.showMessageDialog(null, "Obrigada por utilizar o nosso sistema!");
         }
     }
 
-//    public void imprimir() {
-//        String[] imprimir = {"Imprimir", "Planejar Viagem"};
-//        int desejo = JOptionPane.showOptionDialog(
-//                null,
-//                "Deseja listar suas viagens cadastradas?",
-//                "Planejador de Viagem",
-//                JOptionPane.DEFAULT_OPTION,
-//                JOptionPane.QUESTION_MESSAGE,
-//                null,
-//                imprimir,
-//                imprimir[0]);
-//
-//        if (desejo == 0) {
-//            JOptionPane.showMessageDialog(null, "Nome:" + nome);
-//            JOptionPane.showMessageDialog(null, "Data:" + data);
-//            JOptionPane.showMessageDialog(null, "Dias de viagem:" + dias);
-//            JOptionPane.showMessageDialog(null, "Valor gasto em cada dia:" + valor);
-//
-//        } else if (desejo == 1) {
-//            menu();
-//        }
-//    }
+    public void imprimir() {
+        String[] imprimir = {"Imprimir", "Planejar Viagem"};
+        int desejo = JOptionPane.showOptionDialog(
+                null,
+                "Deseja listar suas viagens cadastradas?",
+                "Planejador de Viagem",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                imprimir,
+                imprimir[0]);
+
+        if (desejo == 0) {
+            JOptionPane.showMessageDialog(null, nome + "sua viagem será dia" + data);
+            JOptionPane.showMessageDialog(null, "Dias de viagem: " + dias);
+            JOptionPane.showMessageDialog(null, "Valor gasto em cada dia: " + valor);
+
+        } else if (desejo == 1) {
+            menu();
+        }
+    }
 }
